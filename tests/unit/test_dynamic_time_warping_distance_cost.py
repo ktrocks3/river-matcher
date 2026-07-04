41c9ba08e7ef580c2c994c496764777028468fad
Kishan Thakurani <kishanthakurani2003@gmail.com>
2026-07-04 15:05:44 +0200

    WIP: automatic save 2026-07-04 15:05:44

--- tests/unit/test_dynamic_time_warping_distance_cost.py
+++ tests/unit/test_dynamic_time_warping_distance_cost.py
@@ -20,10 +20,19 @@ def make_edge(edge_id: int, u: int, v: int, points: list[tuple[float, float]], )
 
 def make_graphs() -> tuple[JunctionGraph, JunctionGraph]:
     source = JunctionGraph(name="source", coordinates={1: (0.0, 0.0), 2: (2.0, 0.0), },
+                           edges=(make_edge(0, 1, 2, [(0.0, 0.0), (2.0, 0.0), ], ), make_edge(1, 1, 2, [(0.0, 0.0), (1.0, 1.0), (2.0, 0.0), ], ),), )
+    target = JunctionGraph(name="target", coordinates={10: (0.0, 0.0), 20: (1.0, 1.0), 30: (2.0, 0.0), 40: (0.0, 1.0), 50: (2.0, 1.0), 60: (10.0, 0.0), 70: (11.0, 0.0), },
+                           edges=(make_edge(10, 10, 20, [(0.0, 0.0), (1.0, 1.0), ], ), make_edge(11, 20, 30, [(1.0, 1.0), (2.0, 0.0), ], ),
+                                  make_edge(12, 40, 50, [(0.0, 1.0), (2.0, 1.0), ], ), make_edge(13, 60, 70, [(10.0, 0.0), (11.0, 0.0), ], ),), )
+
+    return source, target
+
+
+def make_graphs() -> tuple[JunctionGraph, JunctionGraph]:
+    source = JunctionGraph(name="source", coordinates={1: (0.0, 0.0), 2: (2.0, 0.0), },
         edges=(make_edge(0, 1, 2, [(0.0, 0.0), (2.0, 0.0), ], ), make_edge(1, 1, 2, [(0.0, 0.0), (1.0, 1.0), (2.0, 0.0), ], ),), )
     target = JunctionGraph(name="target", coordinates={10: (0.0, 0.0), 20: (1.0, 1.0), 30: (2.0, 0.0), 40: (0.0, 1.0), 50: (2.0, 1.0), 60: (10.0, 0.0), 70: (11.0, 0.0), },
         edges=(make_edge(10, 10, 20, [(0.0, 0.0), (1.0, 1.0), ], ), make_edge(11, 20, 30, [(1.0, 1.0), (2.0, 0.0), ], ), make_edge(12, 40, 50, [(0.0, 1.0), (2.0, 1.0), ], ),
                make_edge(13, 60, 70, [(10.0, 0.0), (11.0, 0.0), ], ),), )
 
     return source, target
-
