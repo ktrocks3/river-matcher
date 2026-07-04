d857be86063deedfecefbe45ce8fbda597c33cb8
Kishan Thakurani <kishanthakurani2003@gmail.com>
2026-07-04 18:38:49 +0200

    WIP: automatic save 2026-07-04 18:38:49

--- src/river_matcher/decomposition.py
+++ src/river_matcher/decomposition.py
@@ -14,9 +14,11 @@ type OwnedEdge = tuple[int, int, int]
 type OwnedEdgePosition = tuple[int, int, int]
 type ChildPositions = tuple[Bag, tuple[int, ...]]
 
+
 class TreewidthHeuristic(StrEnum):
     MINIMUM_FILL_IN = "minimum_fill_in"
     MINIMUM_DEGREE = "minimum_degree"
+
 
 @dataclass(frozen=True, slots=True)
 class BagPlan:
@@ -28,6 +30,33 @@ class BagPlan:
     child_positions: tuple[ChildPositions, ...]
     owned_edge_positions: tuple[OwnedEdgePosition, ...]
 
+
 @dataclass(frozen=True, slots=True)
 class SourceDecomposition:
     """Rooted tree decomposition and source-edge ownership information."""
+    width: int
+    heuristic: TreewidthHeuristic
+    minimum_fill_width: int
+    minimum_degree_width: int
+    root: Bag
+    bags: tuple[Bag, ...]
+    tree_edges: tuple[TreeEdge, ...]
+    parent: Mapping[Bag, Bag | None]
+    children: Mapping[Bag, tuple[Bag, ...]]
+    depth: Mapping[Bag, int]
+    postorder: tuple[Bag, ...]
+    owned_edges: Mapping[Bag, tuple[OwnedEdge, ...]]
+    bag_plans: Mapping[Bag, BagPlan]
+
+    @property
+    def bag_count(self) -> int:
+        return len(self.bags)
+
+    @property
+    def maximum_bag_size(self) -> int:
+        return self.width + 1
+
+def _bag_key(bag: Bag) -> tuple[int, ...]:
+    return tuple(sorted(bag))
+
+def _canonical_tree_edge(first:Bag, second: Bag) -> TreeEdge:
